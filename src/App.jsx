5d5e114a53a27e6479cf17bd2546c0629be9fd92
luisfe0604 <106082104+luisfe0604@users.noreply.github.com>
2026-06-08 14:55:17 -0300

    add api remake front

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
@@ -11,26 +11,25 @@ import ProtectedRoute from "../routes/ProtectedRoute";
 
 export default function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
+    <Routes>
+      <Route path="/login" element={<Login />} />
 
-        <Route
-          element={
-            <ProtectedRoute>
-              <AppLayout />
-            </ProtectedRoute>
-          }
-        >
-          <Route path="/" element={<Dashboard />} />
+      <Route
+        element={
+          <ProtectedRoute>
+            <AppLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route path="/" element={<Dashboard />} />
 
-          <Route path="/alunos" element={<Alunos />} />
+        <Route path="/alunos" element={<Alunos />} />
 
-          <Route path="/presencas" element={<Presencas />} />
+        <Route path="/presencas" element={<Presencas />} />
 
-          <Route path="/pagamentos" element={<Pagamentos />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+        <Route path="/pagamentos" element={<Pagamentos />} />
+
+      </Route>
+    </Routes>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
-ReactDOM.createRoot(document.getElementById('root')).render(<BrowserRouter><App/></BrowserRouter>);
+ReactDOM.createRoot(document.getElementById('root')).render(<HashRouter><App/></HashRouter>);
