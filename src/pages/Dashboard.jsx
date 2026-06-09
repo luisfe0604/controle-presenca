3ca81a99922d89ca6b869ee24050cacfa113c24d
luisfe0604 <106082104+luisfe0604@users.noreply.github.com>
2026-06-09 08:23:37 -0300

    add api remake front

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -48,6 +48,13 @@ export default function Dashboard() {
           <strong>{dados?.inadimplentes ?? 0}</strong>
         </Card>
         <Card>
+          <h3>Receita Líquida Prevista</h3>
+
+          <strong>
+            {dados?.receita_prevista - dash?.custo_quadra_estimado - 100}
+          </strong>
+        </Card>
+        <Card>
           <h3>Receita Líquida</h3>
 
           <strong>R$ {(dados?.receita_liquida ?? 0) - 100}</strong>
@@ -58,9 +65,30 @@ export default function Dashboard() {
           <strong>R$ {dados?.valor_a_receber ?? 0}</strong>
         </Card>
         <Card>
+          <h3>Líquido A Receber</h3>
+
+          <strong>
+            R${" "}
+            {dados?.receita_prevista -
+              dados?.custo_quadra_estimado -
+              100 -
+              (dados?.receita_liquida - 100)}
+          </strong>
+        </Card>
+        <Card>
+          <h3>Custo Quadra</h3>
+
+          <strong>R$ {dados?.custo_quadra}</strong>
+        </Card>
+        <Card>
+          <h3>Custo Quadra Previsto</h3>
+
+          <strong>R$ {dados?.custo_quadra_estimado}</strong>
+        </Card>
+        <Card>
           <h3>Sofia</h3>
 
-          <strong>R$ {100.00}</strong>
+          <strong>R$ {100.0}</strong>
         </Card>
       </div>
     </PageContainer>
